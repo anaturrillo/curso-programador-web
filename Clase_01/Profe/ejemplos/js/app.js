365fe57f52b7d1e2c9fd0d9e346de3601aa12548
Ana Turrillo <anaturrillo@gmail.com>
2018-11-20 21:01:31 -0300

    Se agrega ejemplo final visto en clase

---
+++ Clase_01/Profe/ejemplos/js/app.js
@@ -0,0 +1,11 @@
+var nombre = prompt('decime tu nombre');
+
+var edad = prompt('tu edad');
+
+var numero = 6;
+
+var suma = numero + parseInt(edad);
+
+var mensaje = 'Hola ' + nombre + ', en 6 años vas a tener ' + suma + ' años';
+
+alert(mensaje);
